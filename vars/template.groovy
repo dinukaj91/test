f01058ee352991d4b544e8e514dbdc552e4d1083
Dinuka Jayamaha <dinuka@propertyfinder.ae>
2022-11-05 20:52:48 +0400

    Update template.groovy

--- vars/template.groovy
+++ vars/template.groovy
@@ -3,27 +3,7 @@
  * that should be common for all pipelines)
  */
 def call() {   
-
-    pipeline {
-        agent any
-        parameters {
-            string(name: 'myInput', description: 'Some pipeline parameters')
-        }
-        stages {
-            stage('Stage one') {
-                steps {
-                    script {
-                        echo "Parameter from template creation: "
-                    }
-                }
-            }
-            stage('Stage two') {
-                steps {
-                    script {
-                        echo "Job input parameter: "
-                    }
-                }
-            }
-        }
+    freeStyleJob('pf-customer-notifications-build-pr-image') {
+        description 'Build and push pf-customer-notifications docker image'
     }
 }
