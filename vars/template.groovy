082e6b94a928c0acc7d0d7b227cfaa6d5742373a
Dinuka Jayamaha <dinuka@propertyfinder.ae>
2022-11-05 21:43:59 +0400

    Update template.groovy

--- vars/template.groovy
+++ vars/template.groovy
@@ -2,8 +2,28 @@
  * Defines a pipeline template (as a sample with one job parameter 
  * that should be common for all pipelines)
  */
-def call() {   
-    freeStyleJob() {
-        description 'Build and push pf-customer-notifications docker image'
+def createMyStandardDeclarativePipeline() {   
+
+    pipeline {
+        agent any
+        parameters {
+            string(name: 'myInput', description: 'Some pipeline parameters')
+        }
+        stages {
+            stage('Stage one') {
+                steps {
+                    script {
+                        echo "Parameter from template creation: "
+                    }
+                }
+            }
+            stage('Stage two') {
+                steps {
+                    script {
+                        echo "Job input parameter: "
+                    }
+                }
+            }
+        }
     }
 }
