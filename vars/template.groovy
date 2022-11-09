153f0230322f301d4c7e492c8f62d921976d5ef6
Dinuka Jayamaha <dinuka@propertyfinder.ae>
2022-11-09 16:26:32 +0400

    Update template.groovy

--- vars/template.groovy
+++ vars/template.groovy
@@ -2,6 +2,39 @@
  * Defines a pipeline template (as a sample with one job parameter 
  * that should be common for all pipelines)
  */
+
+properties([
+    parameters([
+        [$class: 'CascadeChoiceParameter', 
+            choiceType: 'PT_SINGLE_SELECT',
+            description: 'Select a choice',
+            filterLength: 1,
+            filterable: true,
+            name: 'choice1',
+            referencedParameters: 'ENVIRONMENT',
+            script: [$class: 'GroovyScript',
+                fallbackScript: [
+                    classpath: [], 
+                    sandbox: true, 
+                    script: 'return ["ERROR"]'
+                ],
+                script: [
+                    classpath: [], 
+                    sandbox: true, 
+                    script: """
+                        if (ENVIRONMENT == 'lab') { 
+                            return['aaa','bbb']
+                        }
+                        else {
+                            return['ccc', 'ddd']
+                        }
+                    """.stripIndent()
+                ]
+            ]
+        ]
+    ])
+])
+
 def call() {   
     pipeline {
         agent { label 'ap-slave-ecs' }
