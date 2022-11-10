cf66e14971803645867babe376a6eb36466fac49
Dinuka Jayamaha <dinuka@propertyfinder.ae>
2022-11-10 10:55:00 +0400

    Update template.groovy

--- vars/template.groovy
+++ vars/template.groovy
@@ -21,32 +21,38 @@ def call() {
                                     filterable: false, 
                                     name: 'Env', 
                                     script: [
-                                        import jenkins.model.*
-                                        import hudson.model.*
-                                        import groovy.json.JsonSlurper
+                                        $class: 'GroovyScript',
+                                        script: [
+                                            classpath: [], 
+                                            sandbox: false, 
+                                            script: 
+                                                import jenkins.model.*
+                                                import hudson.model.*
+                                                import groovy.json.JsonSlurper
 
-                                        if (branch.trim()) {
-                                            credentialsId = 'bbfe5765-9add-4d2c-b10f-9b8c16b8cd19'
-                                            def gitHubToken = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-                                                 com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
-                                                    Jenkins.instance,
-                                                    null,
-                                                    null
-                                            ).find{it.id == credentialsId};
+                                                if (branch.trim()) {
+                                                    credentialsId = 'bbfe5765-9add-4d2c-b10f-9b8c16b8cd19'
+                                                    def gitHubToken = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+                                                         com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class,
+                                                            Jenkins.instance,
+                                                            null,
+                                                            null
+                                                    ).find{it.id == credentialsId};
 
-                                            def urlConnect = new URL("https://api.github.com/repos/Propertyfinder/"+repoName+"/commits?sha="+branch+"&per_page=10")
-                                            def connection = urlConnect.openConnection()
-                                            connection.setRequestProperty("Authorization", "token "+gitHubToken.password)
+                                                    def urlConnect = new URL("https://api.github.com/repos/Propertyfinder/"+repoName+"/commits?sha="+branch+"&per_page=10")
+                                                    def connection = urlConnect.openConnection()
+                                                    connection.setRequestProperty("Authorization", "token "+gitHubToken.password)
 
-                                            def result = new JsonSlurper().parseText(connection.content.text)
+                                                    def result = new JsonSlurper().parseText(connection.content.text)
 
-                                            def commits = []
+                                                    def commits = []
 
-                                            for (i = 0; i <result.size; i++) {
-                                                commits.add(result[i]['sha'].take(10))
-                                            }
-                                            return commits
-                                        }
+                                                    for (i = 0; i <result.size; i++) {
+                                                        commits.add(result[i]['sha'].take(10))
+                                                    }
+                                                    return commits
+                                                }
+                                        ]
                                     ]
                                 ]
                             ])
