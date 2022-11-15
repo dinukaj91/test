ede52372e11ba84cabaaef8985c0aca505d03a5b
Dinuka Jayamaha <dinuka@propertyfinder.ae>
2022-11-15 12:16:17 +0400

    Update python.py

--- python.py
+++ python.py
@@ -25,11 +25,6 @@ def my_function(container):
 
 for i, j in zip(x, y):
     client = docker.from_env()
-    print(os.listdir(os.getcwd()))
-    print(os.getcwd())
-    shutil.rmtree("dump", ignore_errors=True)
-    print(os.path.join(os.getcwd(), "dump"))
-    os.mkdir(os.path.join(os.getcwd(), "dump"))
     container = client.containers.run("bchew/dynamodump:latest",
                                       "-m backup --dumpPath /dump "
                                       f" -r ap-southeast-1 -s {i}",
